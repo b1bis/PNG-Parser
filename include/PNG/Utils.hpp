40774543d1d6bfdfbd59f38b7e1bf0fc1ab2acef
sumnayono <colindemol@gmail.com>
2020-06-14 21:09:17 +0200

    Refactoring integer read function

--- include/PNG/Utils.hpp
+++ include/PNG/Utils.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <cstdint>
 #include <array>
+#include <vector>
 
 namespace PNG
 {
@@ -11,6 +12,10 @@ namespace PNG
 		LittleEndian,
 		BigEndian
 	};
+
+	std::uint16_t Read(std::array<std::uint8_t, 2> data);
+	std::uint32_t Read(std::array<std::uint8_t, 4> data);
+	std::uint64_t Read(std::array<std::uint8_t, 8> data);
 
 	std::istream& Read(std::istream& is, std::uint8_t& i);
 	std::istream& Read(std::istream& is, std::uint16_t& i, Endian endian = Endian::BigEndian);
--- src/Utils.cpp
+++ src/Utils.cpp
@@ -2,77 +2,105 @@
 
 /* Unsigned */
 
-std::istream& PNG::Read(std::istream& is, std::uint8_t& i)
+namespace PNG
 {
-	is.read(reinterpret_cast<char*>(&i), 1);
-	return is;
-}	
-
-std::istream& PNG::Read(std::istream& is, std::uint16_t& i, Endian endian)
-{
-	i = 0;
-	std::array<std::uint8_t, 2> data{};
-	if (endian == Endian::LittleEndian)
+	std::uint16_t Read(std::array<std::uint8_t, 2> data)
 	{
-		for (auto it = data.rbegin(); it != data.rend(); ++it)
-			is.read(reinterpret_cast<char*>(&(*it)), 1);
-	}
-	else if (endian == Endian::BigEndian)
-	{
-		for (auto it = data.begin(); it != data.end(); ++it)
-			is.read(reinterpret_cast<char*>(&(*it)), 1);
+		std::uint16_t i = 0;
+		i |= (0xFF00 & static_cast<std::uint16_t>(data[0] << 8));
+		i |= (0x00FF & data[1]);
+		return i;
 	}
 
-	i |= (0xFF00 & static_cast<std::uint16_t>(data[0] << 8));
-	i |= (0x00FF & data[1]);
-	return is;
-}
-
-std::istream& PNG::Read(std::istream& is, std::uint32_t& i, Endian endian)
-{
-	i = 0;
-	std::array<std::uint8_t, 4> data{};
-
-	if (endian == Endian::LittleEndian)
+	std::uint32_t Read(std::array<std::uint8_t, 4> data)
 	{
-		for (auto it = data.rbegin(); it != data.rend(); ++it)
-			is.read(reinterpret_cast<char*>(&(*it)), 1);
-	}
-	else if (endian == Endian::BigEndian)
-	{
-		for (auto it = data.begin(); it != data.end(); ++it)
-			is.read(reinterpret_cast<char*>(&(*it)), 1);
+		std::uint32_t i = 0;
+		i |= (0xFF000000 & (static_cast<std::uint32_t>(data[0]) << 24));
+		i |= (0x00FF0000 & (static_cast<std::uint32_t>(data[1]) << 16));
+		i |= (0x0000FF00 & (static_cast<std::uint32_t>(data[2]) << 8));
+		i |= (0x000000FF & data[3]);
+		return i;
 	}
 
-	i |= (0xFF000000 & (static_cast<std::uint32_t>(data[0]) << 24));
-	i |= (0x00FF0000 & (static_cast<std::uint32_t>(data[1]) << 16));
-	i |= (0x0000FF00 & (static_cast<std::uint32_t>(data[2]) << 8));
-	i |= (0x000000FF & data[3]);
-	return is;
-}
-
-std::istream& PNG::Read(std::istream& is, std::uint64_t& i, Endian endian)
-{
-	i = 0;
-	std::array<std::uint8_t, 8> data{};
-	if (endian == Endian::LittleEndian)
+	std::uint64_t Read(std::array<std::uint8_t, 8> data)
 	{
-		for (auto it = data.rbegin(); it != data.rend(); ++it)
-			is.read(reinterpret_cast<char*>(&(*it)), 1);
-	}
-	else if (endian == Endian::BigEndian)
-	{
-		for (auto it = data.begin(); it != data.end(); ++it)
-			is.read(reinterpret_cast<char*>(&(*it)), 1);
+		std::uint64_t i = 0;
+		i |= (0xFF00000000000000 & (static_cast<std::uint64_t>(data[0]) << 56));
+		i |= (0x00FF000000000000 & (static_cast<std::uint64_t>(data[1]) << 48));
+		i |= (0x0000FF0000000000 & (static_cast<std::uint64_t>(data[2]) << 40));
+		i |= (0x000000FF00000000 & (static_cast<std::uint64_t>(data[3]) << 32));
+		i |= (0x00000000FF000000 & (static_cast<std::uint64_t>(data[4]) << 24));
+		i |= (0x0000000000FF0000 & (static_cast<std::uint64_t>(data[5]) << 16));
+		i |= (0x000000000000FF00 & (static_cast<std::uint64_t>(data[6]) << 8));
+		i |= (0x00000000000000FF & data[7]);
+		return i;
 	}
 
-	i |= (0xFF00000000000000 & (static_cast<std::uint64_t>(data[0]) << 56));
-	i |= (0x00FF000000000000 & (static_cast<std::uint64_t>(data[1]) << 48));
-	i |= (0x0000FF0000000000 & (static_cast<std::uint64_t>(data[2]) << 40));
-	i |= (0x000000FF00000000 & (static_cast<std::uint64_t>(data[3]) << 32));
-	i |= (0x00000000FF000000 & (static_cast<std::uint64_t>(data[4]) << 24));
-	i |= (0x0000000000FF0000 & (static_cast<std::uint64_t>(data[5]) << 16));
-	i |= (0x000000000000FF00 & (static_cast<std::uint64_t>(data[6]) <<  8));
-	i |= (0x00000000000000FF & data[7]);
-	return is;
+	std::istream& Read(std::istream& is, std::uint8_t& i)
+	{
+		is.read(reinterpret_cast<char*>(&i), 1);
+		return is;
+	}
+
+	std::istream& Read(std::istream& is, std::uint16_t& i, Endian endian)
+	{
+		std::array<std::uint8_t, 2> data{};
+		if (endian == Endian::LittleEndian)
+		{
+			for (auto it = data.rbegin(); it != data.rend(); ++it)
+				is.read(reinterpret_cast<char*>(&(*it)), 1);
+		}
+		else if (endian == Endian::BigEndian)
+		{
+			for (auto it = data.begin(); it != data.end(); ++it)
+				is.read(reinterpret_cast<char*>(&(*it)), 1);
+		}
+
+
+		i = Read(data);
+
+		return is;
+	}
+
+	std::istream& Read(std::istream& is, std::uint32_t& i, Endian endian)
+	{
+		i = 0;
+		std::array<std::uint8_t, 4> data{};
+
+		if (endian == Endian::LittleEndian)
+		{
+			for (auto it = data.rbegin(); it != data.rend(); ++it)
+				is.read(reinterpret_cast<char*>(&(*it)), 1);
+		}
+		else if (endian == Endian::BigEndian)
+		{
+			for (auto it = data.begin(); it != data.end(); ++it)
+				is.read(reinterpret_cast<char*>(&(*it)), 1);
+		}
+
+		i = Read(data);
+
+		return is;
+	}
+
+	std::istream& Read(std::istream& is, std::uint64_t& i, Endian endian)
+	{
+		i = 0;
+		std::array<std::uint8_t, 8> data{};
+		if (endian == Endian::LittleEndian)
+		{
+			for (auto it = data.rbegin(); it != data.rend(); ++it)
+				is.read(reinterpret_cast<char*>(&(*it)), 1);
+		}
+		else if (endian == Endian::BigEndian)
+		{
+			for (auto it = data.begin(); it != data.end(); ++it)
+				is.read(reinterpret_cast<char*>(&(*it)), 1);
+		}
+
+		i = Read(data);
+
+		return is;
+	}
+
 }
