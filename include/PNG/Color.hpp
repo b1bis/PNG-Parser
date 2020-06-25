5e2d209095404dfe825a1d8bd59d6f4112a55a57
sumnayono <colindemol@gmail.com>
2020-06-25 19:07:30 +0200

    Add color class to hold computed pixel

---
+++ include/PNG/Color.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "PNG/Utils.hpp"
+
+namespace PNG 
+{
+	// Hold any PNG-8 color
+	class Color
+	{
+	public:
+		Color() = default;
+		Color(std::uint8_t r, std::uint8_t g, std::uint8_t b, std::uint8_t a = 0xFF) : 
+			m_r { r }, m_g { g }, m_b { b }, m_a { a } {}
+		Color(std::uint8_t grayscale, std::uint8_t a = 0xFF) :
+			m_r{ grayscale }, m_g{ grayscale }, m_b{ grayscale }, m_a{ a } {}
+
+		std::uint8_t GetR() const { return m_r; }
+		std::uint8_t GetG() const { return m_g; }
+		std::uint8_t GetB() const { return m_b; }
+		std::uint8_t GetA() const { return m_a; }
+
+	private:
+		std::uint8_t m_r, m_g, m_b, m_a;
+	};
+}
